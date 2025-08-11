e08786bfb94b5bad3a247005b071753516fd774d
Nicholas Copsey <copseynicholas@gmail.com>
2025-08-11 16:40:14 -0500

    working

---
+++ README.md
@@ -0,0 +1,31 @@
+# Inspirations
+
+This package is inspired by the [redoc-express](https://github.com/AungMyoKyaw/redoc-express) package, and is intended to allow developers who are familiar with that package in [express](https://github.com/expressjs/express) to seemlessly use this package with minimal alterations in [Fastify](https://github.com/fastify/fastify).
+
+Much of the implementaion are cloned from redoc-express, just modified for fastify.
+
+# Usage
+
+```ts
+import RedocFastify, { IOptions } from 'redoc-fastify';
+import Fastify from 'fastify';
+
+async function main(...) {
+    const app = Fastify(); // See [fastify documentation](https://fastify.dev/docs/latest/) for more detail.
+    const opts: IOptions = {
+        title: 'Foo',
+        spec_url: '/openapi.json',
+        file_path: './path/to/openapi.json'
+    };
+    const { docsEndpoint, openAPIEndpoint } = RedocFastify(opts);
+
+    app.get('/', docsEndpoint);
+    app.get(`${opts.spec_url}`. openAPIEnpoint); /* Use if you are hosting the documentation on the same server,
+                                                    otherwise this may be omitted. */
+
+    ...
+}
+```
+
+# Development
+
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import fs from 'fs/promises';
+
+export interface IOptions {
+    title: string
+    spec_url: string
+    file_path: string
+    nonce?: string,
+    redocOptions?: object
+}
+
+export default ({
+    title = 'ReDoc',
+    spec_url = "http://petstore.swagger.io/v2/swagger.json",
+    file_path = spec_url
+}: IOptions) => {
+    const opts: IOptions = { title, spec_url, file_path };
+    const formattedHTML = formatHTML(opts);
+    return {
+        docsEndpoint: (request: FastifyRequest, response: FastifyReply) => {
+            response.header('Content-Type', 'text/html');
+            response.send(formattedHTML);
+        },
+        openAPIEndpoint: async (request: FastifyRequest, response: FastifyReply) => {
+            try {
+                new URL(file_path)
+                // This is an internet url, not a file path. Return a 301 to that url.
+                response.status(301).send(file_path);
+            } catch(e) {
+                // This is a file path on the local system, fetch the file and serve the response.
+                try {
+                    const file = await fs.readFile(file_path, { encoding: 'utf8' });
+                    response.send(JSON.parse(file));
+                } catch(e) {
+                    response.status(404).send({ message: "Not found." });
+                }
+            }
+        }
+    }
+}
+
+const formatHTML = ({ title, spec_url, nonce = '', redocOptions = {}}: IOptions) => {
+    return htmlTemplate
+        .replace('[[title]]', title)
+        .replace('[[nonce]]', nonce)
+        .replace('[[spec-url]]', spec_url)
+        .replace('[[options]]', JSON.stringify(redocOptions))
+}
+
+const htmlTemplate = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>[[title]]</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet" />
+    <style>
+      body {
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="redoc-container"></div>
+    <script nonce='[[nonce]]' src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"> </script>
+  </body>
+  <script>
+    Redoc.init(
+      "[[spec-url]]",
+      [[options]],
+      document.getElementById("redoc-container")
+    );
+  </script>
+</html>`;
